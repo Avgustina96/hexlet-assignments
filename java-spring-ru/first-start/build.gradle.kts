d486fc58f962fa365c95b4ea321b78521f742789
@hexlet/cli <support@hexlet.io>
2024-03-06 22:36:35 +0600

    submit java-spring-ru/first-start

--- java-spring-ru/first-start/build.gradle.kts
+++ java-spring-ru/first-start/build.gradle.kts
@@ -3,45 +3,36 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     application
-    id("io.freefair.lombok") version "8.4"
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.4"
-    id("com.github.ben-manes.versions") version "0.50.0"
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("com.github.ben-manes.versions") version "0.48.0"
 }
 
-group = "io.hexlet.blog"
+group = "io.hexlet"
 
 version = "1.0-SNAPSHOT"
 
-application { mainClass.set("io.hexlet.blog.Application") }
+application { mainClass.set("exercise.Application") }
 
 repositories {
-    mavenCentral()
+    maven {
+        url = uri("https://artifactory.raiffeisen.ru/artifactory/repo1/")
+        credentials {
+            username = System.getProperty("artifactory_user")
+            password = System.getProperty("artifactory_password")
+        }
+    }
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-devtools")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-    // implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.28.0")
-
-    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
-    implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
-
-    runtimeOnly("com.h2database:h2:2.2.224")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("net.datafaker:datafaker:2.0.2")
-    implementation("org.instancio:instancio-junit:3.6.0")
-    implementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    implementation("com.github.stefanbirkner:system-lambda:1.2.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.hamcrest:hamcrest:2.2")
 }
 
 tasks.test {
@@ -52,53 +43,3 @@ tasks.test {
         showStandardStreams = true
     }
 }
-
-
-
-
-
-////import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-////import org.gradle.api.tasks.testing.logging.TestLogEvent
-//
-//plugins {
-//    application
-//    id("org.springframework.boot") version "3.2.2"
-//    id("io.spring.dependency-management") version "1.1.3"
-//    id("com.github.ben-manes.versions") version "0.48.0"
-//}
-//
-//group = "io.hexlet"
-//
-//version = "1.0-SNAPSHOT"
-//
-//application { mainClass.set("exercise.Application") }
-//
-//repositories {
-//    maven {
-//        url = uri("https://artifactory.raiffeisen.ru/artifactory/repo1/")
-//        credentials {
-//            username = System.getProperty("artifactory_user")
-//            password = System.getProperty("artifactory_password")
-//        }
-//    }
-//}
-//
-//dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter")
-//    implementation("org.springframework.boot:spring-boot-starter-web")
-//    implementation("org.springframework.boot:spring-boot-devtools")
-//    implementation("com.github.stefanbirkner:system-lambda:1.2.1")
-//    testImplementation("org.springframework.boot:spring-boot-starter-test")
-//    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-//    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-//    testImplementation("org.hamcrest:hamcrest:2.2")
-//}
-//
-//tasks.test {
-//    useJUnitPlatform()
-//    testLogging {
-//        events("failed", "passed", "skipped")
-//        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-//        showStandardStreams = true
-//    }
-//}
